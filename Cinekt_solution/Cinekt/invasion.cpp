23db0f995bdf4011e10978baf0a1b7c66b8cf136
Bence Moharos <moharos89@gmail.com>
2019-08-01 14:04:58 +0200

    Add new method's declarations creat table and creat boxes

--- Cinekt_solution/Cinekt/invasion.cpp
+++ Cinekt_solution/Cinekt/invasion.cpp
@@ -2,11 +2,23 @@
 
 
 
-Invasion::Invasion()
+Invasion::Invasion(cv::VideoCapture cap) : _cap(cap)
 {
+    int _width = _cap.get(cv::CAP_PROP_FRAME_WIDTH);
+    int _height = _cap.get(cv::CAP_PROP_FRAME_HEIGHT);
 }
 
 
 Invasion::~Invasion()
 {
+}
+
+void Invasion::creatGameTable(std::vector<std::vector<int>>* gameTable)
+{
+
+}
+
+void Invasion::creatBoxes(cv::Point point)
+{
+
 }
--- Cinekt_solution/Cinekt/invasion.h
+++ Cinekt_solution/Cinekt/invasion.h
@@ -1,8 +1,24 @@
 #pragma once
+
+#include <iostream>
+#include "objectTracking.h"
+#include "opencv2/core.hpp"
+#include <vector>
+
+
 class Invasion
 {
 public:
-    Invasion();
+    Invasion(cv::VideoCapture cap);
     ~Invasion();
+
+    void creatGameTable(std::vector<std::vector<int>> *gameTable);
+    void creatBoxes(cv::Point point);
+
+private:
+    cv::VideoCapture _cap;
+    int _width;
+    int _hight;
+
 };
 

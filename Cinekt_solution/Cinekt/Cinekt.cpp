352e80a3d6bb323aa66f92f68c3799540aab1d36
Sandor Derda <47214930+sanyi0411@users.noreply.github.com>
2019-08-01 11:32:55 +0200

    Initialize project

---
+++ Cinekt_solution/Cinekt/Cinekt.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <Windows.h>
+#include <mmsystem.h>
+#pragma comment(lib, "WinMM.Lib")
+
+int main()
+{
+
+}
+
